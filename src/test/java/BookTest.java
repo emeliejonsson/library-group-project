b4feab1aeeea60424ca5cf955590e2b3d7ce87c5
Wendela Graf <ac75270@skola.goteborg.se>
2025-12-03 14:06:12 +0100

    adding book and test

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,26 @@
-class BookTest {
+public class BookTest {
+    public static void main(String[] args) {
+        Book book = new Book("Sagan om ringen", "Ingen aning", "1234567890", 3);
+
+        System.out.println(book.getTitle());
+        System.out.println(book.getAuthor());
+        System.out.println(book.getIsbn());
+        System.out.println(book.getCopies());
+
+        book.addCopy(2);
+        System.out.println(book.getCopies());
+
+        boolean removed = book.removeCopy(1);
+        System.out.println(removed);
+        System.out.println(book.getCopies());
+
+        removed = book.removeCopy(10);
+        System.out.println(removed);          
+        System.out.println(book.getCopies());
+
+        System.out.println(book.isAvailable());
+
+        book.removeCopy(4);
+        System.out.println(book.isAvailable());
+    }
 }
