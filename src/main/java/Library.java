ec8fbdd6b869ea1fca2a766eb1adb9e438bcb530
Emelie Jonsson <jonssonemeliemaria@gmail.com>
2025-12-03 13:37:14 +0100

    add method findBook to library class

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,16 +1,27 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Library {
-    private List<String> booksList;
+    private List<Book> booksList;
     private Map<String, ReasonForRemoval> removedBooks = new HashMap<>();
 
-    public Library(List<String> booksList) {
-        this.booksList = booksList;
+
+    public Library() {
+        this.booksList = new ArrayList<>();
     }
 
-    public void addBook(String newBook) {
+    public void findBook(String bookSearch) {
+        for (Book books : this.booksList) {
+            if (books.getTitle().equals(bookSearch)) {
+                System.out.println(books.getTitle());
+                return;
+            } else {
+                System.out.println("Try again");
+            }
+        }
+
+    }
+
+    public void addBook(Book newBook) {
         booksList.add(newBook);
         System.out.println(newBook + " was added to the library catalogue.");
     }
@@ -23,25 +34,29 @@ public class Library {
             System.out.println("Can't find: " + nameOfBook);
         }
     }
-    public List<String> getBooksList() {
+
+    public List<Book> getBooksList() {
         return booksList;
     }
+
     public Map<String, ReasonForRemoval> getRemovedBooks() {
         return removedBooks;
     }
 
+    // find book för att loopa igenom boklistan
+
     /**ANVÄNDA I MAIN, LÄGGS TILL EFTER booksList
-    Library library = new Library(booksList);
+     Library library = new Library(booksList);
 
-    LÄGG TILL ADD/REMOVE I MAIN:
-    String bookToAdd = scanner.nextLine();
-    library.addBook(bookToAdd);
-    break;
+     LÄGG TILL ADD/REMOVE I MAIN:
+     String bookToAdd = scanner.nextLine();
+     library.addBook(bookToAdd);
+     break;
 
-    String bookToRemove = scanner.nextLine();
-    (print: reason for removal)
-    int removalReason = scanner.nextInt();
-    scanner.nextLine();
+     String bookToRemove = scanner.nextLine();
+     (print: reason for removal)
+     int removalReason = scanner.nextInt();
+     scanner.nextLine();
 
      switch (removalReason) {
      case 1: reason = ReasonForRemoval.LOST;
