3956881a4afd58042045b35999b4984407311d17
greenwzrd6 <t4kikun@gmail.com>
2025-12-04 14:16:55 +0100

    fixed pom.xml with plugin, made main menu, removed user valid name method

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,6 +1,5 @@
 
 import org.junit.jupiter.api.Test;
-import se.models.User;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
