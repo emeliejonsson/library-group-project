621e45892f4fe064bb9ec11744202ea12e0370b4
greenwzrd6 <t4kikun@gmail.com>
2025-12-05 19:04:48 +0100

    Main is almost finished, still some bugs tho. Added run.sh to run mvn clean package and then run the jar file

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -1,106 +1,83 @@
 import java.time.*;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Loan
-{
-	private final User user;
-	private final Book book;
-	private final int loanID;
-	private final LocalDate returnDate;
-	private LoanStatus status;
-	private int random;
+public class Loan {
+    private final User user;
+    private final Book book;
+    private final int loanID;
+    private final LocalDate returnDate;
+    private LoanStatus status;
+    private int random;
 
-	public Loan(int loanID,
-	            User user,
-	            Book book)
-	{
-		random = ThreadLocalRandom.current().nextInt(0, 1000);
-		this.loanID = loanID;
-		this.user = user;
-		this.book = book;
-		if (random == 1000)
-		{
-			status = LoanStatus.BOOKSTOLEN;
-		}
-		else
-		{
-			status = LoanStatus.ACTIVE;
-		}
-		returnDate = LocalDate.now().plusDays(12);
-	}
+    public Loan(int loanID, User user, Book book) {
+        random = ThreadLocalRandom.current().nextInt(0, 1000);
+        this.loanID = loanID;
+        this.user = user;
+        this.book = book;
+        if (random == 1000) {
+            status = LoanStatus.BOOKSTOLEN;
+        } else {
+            status = LoanStatus.ACTIVE;
+        }
+        returnDate = LocalDate.now().plusDays(12);
+    }
 
-	@Override
-	public String toString()
-	{
-		return String.format("""
-		                     Loan ID: %d
-		                     Book: %s
-		                     User: %s
-		                     Due date: %s
-		                     Loan status: %s""", loanID, book.getTitle(),
-		                     user.getName(), returnDate, status);
-	}
+    @Override
+    public String toString() {
+        return String.format("""
+                Låne-id: %d
+                Bok: %s
+                Användare: %s
+                Återlämningsdatum: %s
+                Lånestatus: %s""", loanID, book.getTitle(), user.getName(), returnDate, status);
+    }
 
-	public void endLoan()
-	{
-		status = LoanStatus.OLD;
-	}
+    public void endLoan() {
+        status = LoanStatus.OLD;
+    }
 
-	public void setRandom(int random)
-	{
-		this.random = random;
-		if (random == 1000)
-		{
-			this.status = LoanStatus.BOOKSTOLEN;
-		}
-	}
+    public void setRandom(int random) {
+        this.random = random;
+        if (random == 1000) {
+            this.status = LoanStatus.BOOKSTOLEN;
+        }
+    }
 
-	public User getUser()
-	{
-		return user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public Book getBook()
-	{
-		return book;
-	}
+    public Book getBook() {
+        return book;
+    }
 
-	public int getLoanID()
-	{
-		return loanID;
-	}
+    public int getLoanID() {
+        return loanID;
+    }
 
-	public LocalDate getReturnDate()
-	{
-		return returnDate;
-	}
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
 
-	public LoanStatus getStatus()
-	{
-		return status;
-	}
+    public LoanStatus getStatus() {
+        return status;
+    }
 
-	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + loanID;
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + loanID;
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Loan other = (Loan) obj;
-		if (loanID != other.loanID)
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Loan other = (Loan) obj;
+        if (loanID != other.loanID) return false;
+        return true;
+    }
 }
