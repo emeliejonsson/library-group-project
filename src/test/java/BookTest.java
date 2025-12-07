059ccf79f6f6ad69b2d8dd80b65a8e39cbf54a6b
Wendela Graf <ac75270@skola.goteborg.se>
2025-12-07 21:12:17 +0100

    Adding testclasses to my classes

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,26 +1,57 @@
+import org.junit.Test;
+
+import se.yrgo.project.Book;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.*;
+
 public class BookTest {
-    public static void main(String[] args) {
-        Book book = new Book("Sagan om ringen", "Ingen aning", "1234567890", 3);
 
-        System.out.println(book.getTitle());
-        System.out.println(book.getAuthor());
-        System.out.println(book.getIsbn());
-        System.out.println(book.getCopies());
+    @Test
+    public void testConstructorAndGetters() {
+        Book book = new Book("Sagan om ringen", "Tolkien", "1234567890", 3);
 
+        assertEquals("Sagan om ringen", book.getTitle());
+        assertEquals("Tolkien", book.getAuthor());
+        assertEquals("1234567890", book.getIsbn());
+        assertEquals(3, book.getCopies());
+    }
+
+    @Test
+    public void testAddCopy() {
+        Book book = new Book("Test", "Author", "111", 3);
         book.addCopy(2);
-        System.out.println(book.getCopies());
+        assertEquals(5, book.getCopies());
+    }
 
-        boolean removed = book.removeCopy(1);
-        System.out.println(removed);
-        System.out.println(book.getCopies());
+    @Test
+    public void testAddCopyThrowsExceptionForZeroOrNegative() {
+        Book book = new Book("Test", "Author", "111", 3);
+        assertThrows(IllegalArgumentException.class, () -> book.addCopy(0));
+        assertThrows(IllegalArgumentException.class, () -> book.addCopy(-1));
+    }
 
-        removed = book.removeCopy(10);
-        System.out.println(removed);          
-        System.out.println(book.getCopies());
+    @Test
+    public void testRemoveCopy() {
+        Book book = new Book("Test", "Author", "111", 3);
+        boolean result = book.removeCopy(2);
+        assertTrue(result);
+        assertEquals(1, book.getCopies());
+    }
 
-        System.out.println(book.isAvailable());
+    @Test
+    public void testRemoveCopyFailsWhenNotEnoughCopies() {
+        Book book = new Book("Test", "Author", "111", 1);
+        boolean result = book.removeCopy(5);
+        assertFalse(result);
+        assertEquals(1, book.getCopies());
+    }
 
-        book.removeCopy(4);
-        System.out.println(book.isAvailable());
+    @Test
+    public void testIsAvailable() {
+        Book book = new Book("Test", "Author", "111", 1);
+        assertTrue(book.isAvailable());
+        book.removeCopy(1);
+        assertFalse(book.isAvailable());
     }
 }
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,2 +1,18 @@
-class UserTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UserTest {
+    private final User user1;
+
+    public UserTest() {
+        user1 = new User("sadra");
+    }
+
+    @Test
+    public void testAddUser() {
+        assertNotNull(user1.getUserID(), "User-ID cant be null");
+        assertEquals(10, user1.getUserID().length(), "User-ID must have length 10, meaning name input is in the wrong format.");
+    }
 }
