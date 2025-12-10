882d5e7aa19f2ae8428b534aafbc23bcc6d40f7d
greenwzrd6 <t4kikun@gmail.com>
2025-12-10 09:51:11 +0100

    cleaned up run.sh and pom.xml and added package.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
+if [[ ! -d "target/" || ! $(compgen -G "target/*.jar") ]]
+then
 mvn clean package
+fi
 
 cd target/
 
-java -jar *.jar
+java -jar ./*.jar
